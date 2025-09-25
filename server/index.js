3f5544971280080c043823d0c6f0aec6c5a29dd1
Janeesha Pabasara <IT23599604@my.sliit.lk>
2025-09-25 15:05:40 +0530

    Fix tailwind issue

--- server/index.js
+++ server/index.js
@@ -67,11 +67,8 @@ app.use((req, res, next) => {
         throw err;
     });
 
-    // ALWAYS serve the app on the port specified in the environment variable PORT
-    // Other ports are firewalled. Default to 5000 if not specified.
-    // this serves both the API and the client.
-    // It is the only port that is not firewalled.
-    const port = parseInt(process.env.PORT || '5001', 10);
+    // Use port 5000 to match vite.config.js
+    const port = parseInt(process.env.PORT || '5000', 10);
     server.listen(port, "0.0.0.0", () => {
         log(`serving on port ${port}`);
     });
